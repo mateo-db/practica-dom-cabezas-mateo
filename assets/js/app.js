045646598df38baf911e7e54df93e3e9f3aca484
mateo-db <cabezasmateo752@gmail.com>
2026-05-31 20:00:05 -0300

    feat: se implementó renderización de cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// declaramos todas las variables necesarias
 const rowCards = document.querySelector("#rowCarta")
 const searchName = document.querySelector("#buscarNombre")
 const inputUserName = document.querySelector("#inputUsuario")
@@ -7,6 +8,7 @@ const nameSubmit = document.querySelector("#nombrePersonaje")
 const urlSubmit = document.querySelector("#urlPersonaje")
 const btnSubmit = document.querySelector("#btnSubirPj")
 
+// insertamos el arreglo de pjs
 const personajes = [
 { id: 1, nombre: "A-Bomb", imagen:
 "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/1-a-bomb.jpg" },
@@ -19,3 +21,27 @@ const personajes = [
 { id: 5, nombre: "Abraxas", imagen:
 "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/5-abraxas.jpg" },
 ];
+
+// console.log(personajes)
+//acá se declara la variable funcion cargarPersonajes, donde adentro se ejecutará un forEach que irá recorriendo cada personaje del arreglo, e irá construyendo las cards segun su info respectiva
+const cargarPersonajes = (arregloPj) => {
+    rowCards.innerHTML = ""
+
+    personajes.forEach((personaje) => {
+        rowCards.innerHTML +=`
+            <div class="col-2 mt-4 mb-4" dataset="${personaje.id}">
+                <div class="card" style="width: 10rem;">
+                    <img src="${personaje.imagen}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${personaje.nombre}</h5>
+                        <a href="#" class="btn btn-primary" id="btnDel">Eliminar</a>
+                    </div>
+                </div>
+            </div>
+        `
+    });
+
+}
+
+// aca se llama a la funcion cargarPersonajes
+cargarPersonajes()
